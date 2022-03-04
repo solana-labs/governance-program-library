82cff4b3c74fc19c589d527bc24059bcd4b1bb29
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-03-04 23:25:54 +0100

    Update voter weight record (#6)
    * wip: update_voter_weight_record
    
    * wip: update_voter_weight_record

--- programs/nft-voter/src/instructions/mod.rs
+++ programs/nft-voter/src/instructions/mod.rs
@@ -9,3 +9,6 @@ mod create_voter_weight_record;
 
 pub use create_max_voter_weight_record::*;
 mod create_max_voter_weight_record;
+
+pub use update_voter_weight_record::*;
+mod update_voter_weight_record;
