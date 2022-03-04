b79a155f85ab13225022c166dfefc1a60d2d486a
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-03-04 23:25:41 +0100

    wip: update_voter_weight_record

--- programs/nft-voter/src/instructions/update_voter_weight_record.rs
+++ programs/nft-voter/src/instructions/update_voter_weight_record.rs
@@ -23,30 +23,23 @@ pub struct UpdateVoterWeightRecord<'info> {
 }
 
 pub fn update_voter_weight_record(
-    _ctx: Context<UpdateVoterWeightRecord>,
+    ctx: Context<UpdateVoterWeightRecord>,
     _realm: Pubkey,
     _governing_token_mint: Pubkey,
     _governing_token_owner: Pubkey,
 ) -> Result<()> {
-    // let realm = get_realm_data(
-    //     &ctx.accounts.registrar.governance_program_id,
-    //     &ctx.accounts.realm,
-    // )?;
-    // realm.assert_is_valid_governing_token_mint(&ctx.accounts.realm_governing_token_mint.key())?;
+    // TODO: Validate registrar vs VoterWeightRecord
+    // TODO: Validate governing_token_owner
 
-    // // TODO: Assert register matched realm and  realm_governing_token_mint
+    let voter_weight_record = &mut ctx.accounts.voter_weight_record;
 
-    // let voter_weight_record = &mut ctx.accounts.voter_weight_record;
+    // TODO: Pass NFTs and evaluate the same way as Vote does
+    voter_weight_record.voter_weight = 10;
 
-    // voter_weight_record.account_discriminator =
-    //     spl_governance_addin_api::voter_weight::VoterWeightRecord::ACCOUNT_DISCRIMINATOR;
-
-    // voter_weight_record.realm = ctx.accounts.realm.key();
-    // voter_weight_record.governing_token_mint = ctx.accounts.realm_governing_token_mint.key();
-    // voter_weight_record.governing_token_owner = governing_token_owner;
-
-    // // Set expiry to expired
-    // voter_weight_record.voter_weight_expiry = Some(0);
+    // Record is only valid as of the current slot
+    voter_weight_record.voter_weight_expiry = Some(Clock::get()?.slot);
+    voter_weight_record.weight_action = None;
+    voter_weight_record.weight_action_target = None;
 
     Ok(())
 }
