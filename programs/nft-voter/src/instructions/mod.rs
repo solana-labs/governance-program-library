a5bacc71e001f894898fcb6d83f42e79d59d7c29
urkes <uros.sosevic@gmail.com>
2022-03-04 23:22:39 +0100

    fix: update lib and mod

--- programs/nft-voter/src/instructions/mod.rs
+++ programs/nft-voter/src/instructions/mod.rs
@@ -4,9 +4,6 @@ mod configure_collection;
 pub use create_registrar::*;
 mod create_registrar;
 
-// pub use vote_with_nft::*;
-// mod vote_with_nft;
-
 pub use create_voter_weight_record::*;
 mod create_voter_weight_record;
 
