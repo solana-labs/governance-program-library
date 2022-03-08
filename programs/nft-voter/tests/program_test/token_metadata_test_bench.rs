45e24915df7dcb318995e32d252045fb328fa898
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-03-08 01:32:26 +0000

    wip: create NFT with verified collection

--- programs/nft-voter/tests/program_test/token_metadata_test_bench.rs
+++ programs/nft-voter/tests/program_test/token_metadata_test_bench.rs
@@ -40,6 +40,78 @@ impl TokenMetadataTestBench {
         let nft_owner = self.bench.context.borrow().payer.pubkey();
         let payer = self.bench.context.borrow().payer.pubkey();
 
+        // Create collection
+        let coll_mint_cookie = self.bench.with_mint().await;
+        let coll_nft_account_cookie = self
+            .bench
+            .with_tokens(&coll_mint_cookie, &nft_owner, 1)
+            .await;
+
+        let coll_metadata_seeds = &[
+            b"metadata".as_ref(),
+            self.program_id.as_ref(),
+            &coll_mint_cookie.address.as_ref(),
+        ];
+        let (coll_metadata_address, _) =
+            Pubkey::find_program_address(coll_metadata_seeds, &self.program_id);
+
+        let coll_name = "TestNFT".to_string();
+        let coll_symbol = "NFT".to_string();
+        let coll_uri = "URI".to_string();
+
+        let create_coll_metadata_ix = mpl_token_metadata::instruction::create_metadata_accounts_v2(
+            self.program_id,
+            coll_metadata_address,
+            coll_mint_cookie.address,
+            coll_mint_cookie.mint_authority.pubkey(),
+            nft_owner.clone(),
+            nft_owner.clone(),
+            coll_name,
+            coll_symbol,
+            coll_uri,
+            None,
+            10,
+            false,
+            false,
+            None,
+            None,
+        );
+
+        self.bench
+            .process_transaction(
+                &[create_coll_metadata_ix],
+                Some(&[&coll_mint_cookie.mint_authority]),
+            )
+            .await;
+
+        let master_edition_seeds = &[
+            b"metadata".as_ref(),
+            self.program_id.as_ref(),
+            coll_mint_cookie.address.as_ref(),
+            b"edition".as_ref(),
+        ];
+        let (master_edition_address, _) =
+            Pubkey::find_program_address(master_edition_seeds, &self.program_id);
+
+        let create_master_edition_ix = mpl_token_metadata::instruction::create_master_edition_v3(
+            self.program_id,
+            master_edition_address,
+            coll_mint_cookie.address,
+            nft_owner,
+            coll_mint_cookie.mint_authority.pubkey(),
+            coll_metadata_address,
+            payer,
+            Some(0),
+        );
+
+        self.bench
+            .process_transaction(
+                &[create_master_edition_ix],
+                Some(&[&coll_mint_cookie.mint_authority]),
+            )
+            .await;
+
+        // Crate NFT
         let mint_cookie = self.bench.with_mint().await;
         let nft_account_cookie = self.bench.with_tokens(&mint_cookie, &nft_owner, 1).await;
 
@@ -56,7 +128,7 @@ impl TokenMetadataTestBench {
 
         let collection = Collection {
             verified: false,
-            key: Pubkey::new_unique(),
+            key: coll_mint_cookie.address.clone(),
         };
 
         let create_metadata_ix = mpl_token_metadata::instruction::create_metadata_accounts_v2(
@@ -70,7 +142,7 @@ impl TokenMetadataTestBench {
             symbol,
             uri,
             None,
-            0,
+            10,
             false,
             false,
             Some(collection),
@@ -81,43 +153,20 @@ impl TokenMetadataTestBench {
             .process_transaction(&[create_metadata_ix], Some(&[&mint_cookie.mint_authority]))
             .await;
 
-        let master_edition_seeds = &[
-            b"metadata".as_ref(),
-            self.program_id.as_ref(),
-            mint_cookie.address.as_ref(),
-            b"edition".as_ref(),
-        ];
-        let (master_edition_address, _) =
-            Pubkey::find_program_address(master_edition_seeds, &self.program_id);
-
-        let create_master_edition_ix = mpl_token_metadata::instruction::create_master_edition_v3(
+        let verify_collection = mpl_token_metadata::instruction::verify_collection(
             self.program_id,
-            master_edition_address,
-            mint_cookie.address,
-            nft_owner,
-            mint_cookie.mint_authority.pubkey(),
             metadata_address,
+            nft_owner,
             payer,
+            coll_mint_cookie.address,
+            coll_metadata_address,
+            master_edition_address,
             None,
         );
 
         self.bench
-            .process_transaction(
-                &[create_master_edition_ix],
-                Some(&[&mint_cookie.mint_authority]),
-            )
+            .process_transaction(&[verify_collection], None)
             .await;
-
-        // let verify_collection = mpl_token_metadata::instruction::verify_collection(
-        //     self.program_id,
-        //     metadata_address,
-        //     collection_authority.pubkey(),
-        //     context.payer.pubkey().clone(),
-        //     collection_mint,
-        //     collection,
-        //     collection_master_edition_account,
-        //     collection_authority_record,
-        // );
 
         NftCookie {
             address: nft_account_cookie.address,
