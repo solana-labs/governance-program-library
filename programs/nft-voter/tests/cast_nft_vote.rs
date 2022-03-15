e6c1abb77d6efc9399f05ab09ef1e095b3eb7cc4
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-03-14 23:57:28 -0400

    chore: test_cast_nft_vote_with_invalid_collection_error

--- programs/nft-voter/tests/cast_nft_vote.rs
+++ programs/nft-voter/tests/cast_nft_vote.rs
@@ -441,3 +441,67 @@ async fn test_cast_nft_vote_with_invalid_owner_error() -> Result<(), TransportEr
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_cast_nft_vote_with_invalid_collection_error() -> Result<(), TransportError> {
+    // Arrange
+    let mut nft_voter_test = NftVoterTest::start_new().await;
+
+    let realm_cookie = nft_voter_test.governance.with_realm().await?;
+
+    let registrar_cookie = nft_voter_test.with_registrar(&realm_cookie).await?;
+
+    let nft_collection_cookie = nft_voter_test.token_metadata.with_nft_collection().await?;
+
+    let max_voter_weight_record_cookie = nft_voter_test
+        .with_max_voter_weight_record(&registrar_cookie)
+        .await?;
+
+    nft_voter_test
+        .with_collection(
+            &registrar_cookie,
+            &nft_collection_cookie,
+            &max_voter_weight_record_cookie,
+            Some(ConfigureCollectionArgs {
+                weight: 10,
+                size: 20,
+            }),
+        )
+        .await?;
+
+    let proposal_cookie = nft_voter_test
+        .governance
+        .with_proposal(&realm_cookie)
+        .await?;
+
+    let voter_cookie = nft_voter_test.bench.with_wallet().await;
+
+    let mut voter_weight_record_cookie = nft_voter_test
+        .with_voter_weight_record(&registrar_cookie, &voter_cookie)
+        .await?;
+
+    let nft_collection_cookie2 = nft_voter_test.token_metadata.with_nft_collection().await?;
+
+    let nft_cookie = nft_voter_test
+        .token_metadata
+        .with_nft_v2(&nft_collection_cookie2, &voter_cookie, true)
+        .await?;
+
+    // Act
+    let err = nft_voter_test
+        .cast_nft_vote(
+            &registrar_cookie,
+            &voter_weight_record_cookie,
+            &proposal_cookie,
+            &voter_cookie,
+            &[&nft_cookie],
+        )
+        .await
+        .err()
+        .unwrap();
+
+    // Assert
+    assert_nft_voter_err(err, NftVoterError::CollectionNotFound);
+
+    Ok(())
+}
