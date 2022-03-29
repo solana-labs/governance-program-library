8e609376d868db1a5703e409ccd5fbeff5b10739
silas <95582913+silas-x@users.noreply.github.com>
2022-03-29 20:26:45 +0100

    fmt: remove trailing space

--- programs/nft-voter/src/instructions/cast_nft_vote.rs
+++ programs/nft-voter/src/instructions/cast_nft_vote.rs
@@ -20,7 +20,7 @@ pub struct CastNftVote<'info> {
 
     #[account(
         mut,
-        constraint = voter_weight_record.realm == registrar.realm 
+        constraint = voter_weight_record.realm == registrar.realm
         @ NftVoterError::InvalidVoterWeightRecordRealm,
 
         constraint = voter_weight_record.governing_token_mint == registrar.governing_token_mint
