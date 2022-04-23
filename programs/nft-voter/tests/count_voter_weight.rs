3bfc58c14d12332099de324de3e85c4f263883c9
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-04-23 01:23:53 +0100

    chore: test_count_voter_weight_with_nft_already_voted_error

--- programs/nft-voter/tests/count_voter_weight.rs
+++ programs/nft-voter/tests/count_voter_weight.rs
@@ -1,5 +1,7 @@
 use crate::program_test::nft_voter_test::ConfigureCollectionArgs;
-use program_test::nft_voter_test::NftVoterTest;
+use gpl_nft_voter::error::NftVoterError;
+
+use program_test::{nft_voter_test::NftVoterTest, tools::assert_nft_voter_err};
 use solana_program_test::*;
 use solana_sdk::transport::TransportError;
 
@@ -68,20 +70,90 @@ async fn test_count_voter_weight() -> Result<(), TransportError> {
 
     assert_eq!(voter_weight_counter_cookie.account, voter_weight_counter);
 
-    // let voter_weight_record = nft_voter_test
-    //     .get_voter_weight_record(&voter_weight_record_cookie.address)
-    //     .await;
+    let nft_vote_record_cookies = voter_weight_counter_cookie.nft_vote_record_cookies;
 
-    // assert_eq!(voter_weight_record.voter_weight, 10);
-    // assert_eq!(voter_weight_record.voter_weight_expiry, Some(clock.slot));
-    // assert_eq!(
-    //     voter_weight_record.weight_action,
-    //     Some(VoterWeightAction::CastVote.into())
-    // );
-    // assert_eq!(
-    //     voter_weight_record.weight_action_target,
-    //     Some(proposal_cookie.address)
-    // );
+    let nft_vote_record = nft_voter_test
+        .get_nf_vote_record_account(&nft_vote_record_cookies[0].address)
+        .await;
+
+    assert_eq!(nft_vote_record_cookies[0].account, nft_vote_record);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_count_voter_weight_with_nft_already_voted_error() -> Result<(), TransportError> {
+    // Arrange
+    let mut nft_voter_test = NftVoterTest::start_new().await;
+
+    let realm_cookie = nft_voter_test.governance.with_realm().await?;
+
+    let registrar_cookie = nft_voter_test.with_registrar(&realm_cookie).await?;
+
+    let nft_collection_cookie = nft_voter_test.token_metadata.with_nft_collection().await?;
+
+    let max_voter_weight_record_cookie = nft_voter_test
+        .with_max_voter_weight_record(&registrar_cookie)
+        .await?;
+
+    let collection_config_cookie = nft_voter_test
+        .with_collection(
+            &registrar_cookie,
+            &nft_collection_cookie,
+            &max_voter_weight_record_cookie,
+            Some(ConfigureCollectionArgs {
+                weight: 10,
+                size: 20,
+            }),
+        )
+        .await?;
+
+    let voter_cookie = nft_voter_test.bench.with_wallet().await;
+
+    let voter_weight_record_cookie = nft_voter_test
+        .with_voter_weight_record(&registrar_cookie, &voter_cookie)
+        .await?;
+
+    let proposal_cookie = nft_voter_test
+        .governance
+        .with_proposal(&realm_cookie)
+        .await?;
+
+    let nft_cookie1 = nft_voter_test
+        .token_metadata
+        .with_nft_v2(&nft_collection_cookie, &voter_cookie, None)
+        .await?;
+
+    nft_voter_test
+        .count_voter_weight(
+            &registrar_cookie,
+            &voter_weight_record_cookie,
+            &proposal_cookie,
+            &voter_cookie,
+            &[&nft_cookie1],
+            &collection_config_cookie,
+        )
+        .await?;
+
+    nft_voter_test.bench.advance_clock().await;
+
+    // Act
+
+    let err = nft_voter_test
+        .count_voter_weight(
+            &registrar_cookie,
+            &voter_weight_record_cookie,
+            &proposal_cookie,
+            &voter_cookie,
+            &[&nft_cookie1],
+            &collection_config_cookie,
+        )
+        .await
+        .err()
+        .unwrap();
+
+    // Assert
+    assert_nft_voter_err(err, NftVoterError::NftAlreadyVoted);
 
     Ok(())
 }
