07411b816323eed4bb83de5d6833d8a68db9edb6
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-04-23 02:13:40 +0100

    chore: test_count_voter_weight_with_invalid_registrar_in_subsequent_call_error

--- programs/nft-voter/tests/count_voter_weight.rs
+++ programs/nft-voter/tests/count_voter_weight.rs
@@ -334,3 +334,87 @@ async fn test_count_voter_weight_with_invalid_proposal_in_subsequent_call_error(
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_count_voter_weight_with_invalid_registrar_in_subsequent_call_error(
+) -> Result<(), TransportError> {
+    // Arrange
+    let mut nft_voter_test = NftVoterTest::start_new().await;
+
+    let realm_cookie = nft_voter_test.governance.with_realm().await?;
+
+    let registrar_cookie = nft_voter_test.with_registrar(&realm_cookie).await?;
+
+    let nft_collection_cookie = nft_voter_test.token_metadata.with_nft_collection().await?;
+
+    let max_voter_weight_record_cookie = nft_voter_test
+        .with_max_voter_weight_record(&registrar_cookie)
+        .await?;
+
+    let collection_config_cookie = nft_voter_test
+        .with_collection(
+            &registrar_cookie,
+            &nft_collection_cookie,
+            &max_voter_weight_record_cookie,
+            Some(ConfigureCollectionArgs {
+                weight: 10,
+                size: 20,
+            }),
+        )
+        .await?;
+
+    let voter_cookie = nft_voter_test.bench.with_wallet().await;
+
+    let voter_weight_record_cookie = nft_voter_test
+        .with_voter_weight_record(&registrar_cookie, &voter_cookie)
+        .await?;
+
+    let proposal_cookie = nft_voter_test
+        .governance
+        .with_proposal(&realm_cookie)
+        .await?;
+
+    let nft_cookie1 = nft_voter_test
+        .token_metadata
+        .with_nft_v2(&nft_collection_cookie, &voter_cookie, None)
+        .await?;
+
+    nft_voter_test
+        .count_voter_weight(
+            &registrar_cookie,
+            &voter_weight_record_cookie,
+            &proposal_cookie,
+            &voter_cookie,
+            &[&nft_cookie1],
+            &collection_config_cookie,
+            None,
+        )
+        .await?;
+
+    nft_voter_test.bench.advance_clock().await;
+
+    // Try use registrar from different realm
+    let realm_cookie2 = nft_voter_test.governance.with_realm().await?;
+
+    let registrar_cookie2 = nft_voter_test.with_registrar(&realm_cookie2).await?;
+    // Act
+
+    let err = nft_voter_test
+        .count_voter_weight(
+            &registrar_cookie2,
+            &voter_weight_record_cookie,
+            &proposal_cookie,
+            &voter_cookie,
+            &[&nft_cookie1],
+            &collection_config_cookie,
+            None,
+        )
+        .await
+        .err()
+        .unwrap();
+
+    // Assert
+    assert_nft_voter_err(err, NftVoterError::InvalidRegistrarForVoterWeightCounter);
+
+    Ok(())
+}
