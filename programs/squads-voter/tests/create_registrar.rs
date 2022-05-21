58ac1b9512c14ed627bdf6151daecc43c4ba32e7
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-05-22 01:18:49 +0300

    wip: create_registrar tests

--- programs/squads-voter/tests/create_registrar.rs
+++ programs/squads-voter/tests/create_registrar.rs
@@ -1,7 +1,7 @@
 mod program_test;
 
 use anchor_lang::prelude::Pubkey;
-use gpl_nft_voter::error::NftVoterError;
+use gpl_squads_voter::error::SquadVoterError;
 use program_test::nft_voter_test::NftVoterTest;
 
 use solana_program::instruction::InstructionError;
@@ -45,7 +45,7 @@ async fn test_create_registrar_with_invalid_realm_authority_error() -> Result<()
         .err()
         .unwrap();
 
-    assert_nft_voter_err(err, NftVoterError::InvalidRealmAuthority);
+    assert_nft_voter_err(err, SquadVoterError::InvalidRealmAuthority);
 
     Ok(())
 }
