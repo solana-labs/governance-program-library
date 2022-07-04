22e1ef53408438ea24f1c7a69e5253defbf0d5cf
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-07-04 13:36:28 +0100

    chore: create_voter_weight_record tests

--- programs/realm-voter/tests/create_voter_weight_record.rs
+++ programs/realm-voter/tests/create_voter_weight_record.rs
@@ -1,4 +1,6 @@
 use crate::program_test::realm_voter_test::RealmVoterTest;
+use program_test::tools::assert_ix_err;
+use solana_program::instruction::InstructionError;
 use solana_program_test::*;
 use solana_sdk::transport::TransportError;
 
@@ -31,94 +33,94 @@ async fn test_create_voter_weight_record() -> Result<(), TransportError> {
     Ok(())
 }
 
-// #[tokio::test]
-// async fn test_create_voter_weight_record_with_invalid_realm_error() -> Result<(), TransportError> {
-//     // Arrange
-//     let mut realm_voter_test = NftVoterTest::start_new().await;
+#[tokio::test]
+async fn test_create_voter_weight_record_with_invalid_realm_error() -> Result<(), TransportError> {
+    // Arrange
+    let mut realm_voter_test = RealmVoterTest::start_new().await;
 
-//     let realm_cookie = realm_voter_test.governance.with_realm().await?;
+    let realm_cookie = realm_voter_test.governance.with_realm().await?;
 
-//     let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
+    let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
 
-//     let realm_cookie2 = realm_voter_test.governance.with_realm().await?;
+    let realm_cookie2 = realm_voter_test.governance.with_realm().await?;
 
-//     let voter_cookie = realm_voter_test.bench.with_wallet().await;
+    let voter_cookie = realm_voter_test.bench.with_wallet().await;
 
-//     // Act
-//     let err = realm_voter_test
-//         .with_voter_weight_record_using_ix(&registrar_cookie, &voter_cookie, |i| {
-//             i.accounts[2].pubkey = realm_cookie2.address // Realm
-//         })
-//         .await
-//         .err()
-//         .unwrap();
+    // Act
+    let err = realm_voter_test
+        .with_voter_weight_record_using_ix(&registrar_cookie, &voter_cookie, |i| {
+            i.accounts[2].pubkey = realm_cookie2.address // Realm
+        })
+        .await
+        .err()
+        .unwrap();
 
-//     // Assert
+    // Assert
 
-//     // PDA doesn't match and hence the error is PrivilegeEscalation
-//     assert_ix_err(err, InstructionError::PrivilegeEscalation);
+    // PDA doesn't match and hence the error is PrivilegeEscalation
+    assert_ix_err(err, InstructionError::PrivilegeEscalation);
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// #[tokio::test]
-// async fn test_create_voter_weight_record_with_invalid_mint_error() -> Result<(), TransportError> {
-//     // Arrange
-//     let mut realm_voter_test = NftVoterTest::start_new().await;
+#[tokio::test]
+async fn test_create_voter_weight_record_with_invalid_mint_error() -> Result<(), TransportError> {
+    // Arrange
+    let mut realm_voter_test = RealmVoterTest::start_new().await;
 
-//     let realm_cookie = realm_voter_test.governance.with_realm().await?;
+    let realm_cookie = realm_voter_test.governance.with_realm().await?;
 
-//     let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
+    let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
 
-//     let realm_cookie2 = realm_voter_test.governance.with_realm().await?;
+    let realm_cookie2 = realm_voter_test.governance.with_realm().await?;
 
-//     let voter_cookie = realm_voter_test.bench.with_wallet().await;
+    let voter_cookie = realm_voter_test.bench.with_wallet().await;
 
-//     // Act
-//     let err = realm_voter_test
-//         .with_voter_weight_record_using_ix(&registrar_cookie, &voter_cookie, |i| {
-//             i.accounts[2].pubkey = realm_cookie2.address // Mint
-//         })
-//         .await
-//         .err()
-//         .unwrap();
+    // Act
+    let err = realm_voter_test
+        .with_voter_weight_record_using_ix(&registrar_cookie, &voter_cookie, |i| {
+            i.accounts[2].pubkey = realm_cookie2.address // Mint
+        })
+        .await
+        .err()
+        .unwrap();
 
-//     // Assert
+    // Assert
 
-//     // PDA doesn't match and hence the error is PrivilegeEscalation
-//     assert_ix_err(err, InstructionError::PrivilegeEscalation);
+    // PDA doesn't match and hence the error is PrivilegeEscalation
+    assert_ix_err(err, InstructionError::PrivilegeEscalation);
 
-//     Ok(())
-// }
+    Ok(())
+}
 
-// #[tokio::test]
-// async fn test_create_voter_weight_record_with_already_exists_error() -> Result<(), TransportError> {
-//     // Arrange
-//     let mut realm_voter_test = NftVoterTest::start_new().await;
+#[tokio::test]
+async fn test_create_voter_weight_record_with_already_exists_error() -> Result<(), TransportError> {
+    // Arrange
+    let mut realm_voter_test = RealmVoterTest::start_new().await;
 
-//     let realm_cookie = realm_voter_test.governance.with_realm().await?;
+    let realm_cookie = realm_voter_test.governance.with_realm().await?;
 
-//     let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
+    let registrar_cookie = realm_voter_test.with_registrar(&realm_cookie).await?;
 
-//     let voter_cookie = realm_voter_test.bench.with_wallet().await;
+    let voter_cookie = realm_voter_test.bench.with_wallet().await;
 
-//     realm_voter_test
-//         .with_voter_weight_record(&registrar_cookie, &voter_cookie)
-//         .await?;
+    realm_voter_test
+        .with_voter_weight_record(&registrar_cookie, &voter_cookie)
+        .await?;
 
-//     realm_voter_test.bench.advance_clock().await;
+    realm_voter_test.bench.advance_clock().await;
 
-//     // Act
-//     let err = realm_voter_test
-//         .with_voter_weight_record(&registrar_cookie, &voter_cookie)
-//         .await
-//         .err()
-//         .unwrap();
+    // Act
+    let err = realm_voter_test
+        .with_voter_weight_record(&registrar_cookie, &voter_cookie)
+        .await
+        .err()
+        .unwrap();
 
-//     // Assert
+    // Assert
 
-//     // InstructionError::Custom(0) is returned for TransactionError::AccountInUse
-//     assert_ix_err(err, InstructionError::Custom(0));
+    // InstructionError::Custom(0) is returned for TransactionError::AccountInUse
+    assert_ix_err(err, InstructionError::Custom(0));
 
-//     Ok(())
-// }
+    Ok(())
+}
