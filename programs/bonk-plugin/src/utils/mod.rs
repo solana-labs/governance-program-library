3393502fa0b23c26ff98b8749946a86592239896
0xShuk <shukarsh@gmail.com>
2024-09-17 00:39:08 +0530

    Bonk Voter Plugin

---
+++ programs/bonk-plugin/src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod stake_pool;
+pub mod anchor;
+pub mod stake_deposit_receipt;
---
+++ programs/bonk-plugin/tests/program_test/mod.rs
@@ -0,0 +1,4 @@
+pub mod governance_test;
+pub mod program_test_bench;
+pub mod realm_voter_test;
+pub mod tools;
